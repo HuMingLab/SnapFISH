b986698b093df4b8935211d46c7ea516838f8d08
lindsayhrlee <71450689+lindsayhrlee@users.noreply.github.com>
2023-01-24 10:51:37 -0800

    Update run_SnapFISH.sh

--- run_SnapFISH.sh
+++ run_SnapFISH.sh
@@ -6,12 +6,19 @@
 ############################################################################
 
 
-SnapFISH_dir="/home/leeh7/SnapFiSH/python/"
-input_dir="/home/leeh7/SnapFiSH/python/063022_input_3D_coordinates_129.txt"
-output_dir="/home/leeh7/SnapFiSH/python/output"
-ann_file="/home/leeh7/SnapFiSH/python/063022_input_ann.txt"
-save_pic=1
+SnapFISH_dir="/home/SnapFiSH/python/"
+input_dir="/home/SnapFiSH/python/063022_input_3D_coordinates_129.txt"
+output_dir="/home/SnapFiSH/python/output"
+ann_file="/home/SnapFiSH/python/063022_input_ann.txt"
+save_pic=0
 data_name="129"
+
+########## For 25Kb version ########
+
+rep1_dir="/home/SnapFiSH/101522_mESC_DNAseqFISH+_25Kb_autosomes_268718loci_Rep1.txt"                                                               
+rep2_dir="/home/SnapFiSH//101522_mESC_DNAseqFISH+_25Kb_autosomes_394347loci_Rep2.txt" 
+bin_size=25000
+num_CPUs=10
 
 
 ############################################################################
@@ -23,5 +30,13 @@ if [ ! -d "${output_dir}/tempfile" ]; then
   mkdir ${output_dir}/tempfile
 fi
 
-python ${SnapFISH_dir}/SnapFISH.py -s $SnapFISH_dir -i $input_dir -o $output_dir -a $ann_file -p $save_pic -d $data_name
+
+if [[ $bin_size -eq 25000 ]]; then
+python ${SnapFISH_dir}/SnapFISH25Kb_p.py -s $SnapFISH_dir  -o $output_dir -a $ann_file -d $data_name --rep1 $rep1_dir --rep2 $rep2_dir -n $num_CPUs                     
+fi
+
+if [[ $bin_size -ne 25000 ]]; then
+python ${SnapFISH_dir}/SnapFISH.py -s $SnapFISH_dir -i $input_dir -o $output_dir -a $ann_file -p $save_pic -d $data_name                                                
+fi
+
 
