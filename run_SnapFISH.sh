4c586f96103569192392b677263d94f96749ef66
lindsayhrlee <71450689+lindsayhrlee@users.noreply.github.com>
2023-01-24 11:21:35 -0800

    Update run_SnapFISH.sh

--- run_SnapFISH.sh
+++ run_SnapFISH.sh
@@ -12,12 +12,12 @@ output_dir="/home/SnapFiSH/python/output"
 ann_file="/home/SnapFiSH/python/063022_input_ann.txt"
 save_pic=0
 data_name="129"
+bin_size=5000
 
 ########## For 25Kb version ########
 
 rep1_dir="/home/SnapFiSH/101522_mESC_DNAseqFISH+_25Kb_autosomes_268718loci_Rep1.txt"                                                               
 rep2_dir="/home/SnapFiSH//101522_mESC_DNAseqFISH+_25Kb_autosomes_394347loci_Rep2.txt" 
-bin_size=25000
 num_CPUs=10
 
 
